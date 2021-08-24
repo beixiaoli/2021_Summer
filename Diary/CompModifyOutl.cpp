471872b4b0a6a9b0e316e55f168205ebe491fa9f
beixiaoli <87683342+beixiaoli@users.noreply.github.com>
2021-08-24 09:17:47 +0800

    Update CompModifyOutl.cpp

--- Diary/CompModifyOutl.cpp
+++ Diary/CompModifyOutl.cpp
@@ -10,6 +10,19 @@ for (int i = 1; i < 13; i++) {
 	}
 
 //如果ISO选取不连续
-
+for (int i = 0; i < 14; i++) {
+	if (ISOFlag & (1 << i)) {
+		ISOArray[k] = i;
+		k++;
+	}
+}
+	
+for (int i = 1; i < (k-1); i++) {
+	for (int j = 0; j < 33; j++) {
+		if((denoiseParams->curve_lp0[ISOArray[i]*33+j] > denoiseParams->curve_lp0[ISOArray[i-1]*33+j]) && (denoiseParams->curve_lp0[ISOArray[i]*33+j] > denoiseParams->curve_lp0[ISOArray[i+1]*33+j])) {
+			denoiseParams->curve_lp0[ISOArray[i]*33+j] = (float) (denoiseParams->curve_lp0[ISOArray[i-1]*33+j] + denoiseParams->curve_lp0[ISOArray[i+1]*33+j]) / 2;
+		}
+	}
+}
 
 
